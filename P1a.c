b44487ba3a62d8b2ab34b31ba6de3a0c64808b2e
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-04-18 19:01:56 -0300

    "Hoshen, Actualizar, Etiqueta_falsa, Corregir_etiqueta."

--- P1a.c
+++ P1a.c
@@ -4,12 +4,17 @@
 #include <time.h>
 #include <unistd.h>
 
+#define N     20 //lado de la red simulada
 
 //-------------------------------------------------------------------
 //                     Declaracion de funciones
 //-------------------------------------------------------------------
 void llenar(int *red, int n, float prob); 
 void imprimir(int *red, int n);
+int hoshen (int *red, int n);
+int actualizar(int *red, int *clase, int s, int frag);
+int etiqueta_falsa(int *red, int *clase, int sa, int si);
+int corregir_etiqueta(int *red, int *clase, int n);
 
 int main(){
    //Declaraciones
@@ -17,19 +22,25 @@ int main(){
    float prob;
 
    //Defino
-   n=5;
+   n=N;
    prob=0.5;
    red=malloc(n*n*sizeof(float));
 
    llenar(red,n,prob);
    imprimir(red,n);
+   hoshen(red,n);
+   imprimir(red,n);
 
-
-
+free(red);
+return 0;
 }
 //-----------------------------------------------------------
 //                   Definicion de funciones
 //-----------------------------------------------------------
+//1)Llenar
+
+  //Función que llena la red de nxn con probabilidad p.
+
 void llenar(int *red, int n, float p){
 
 	/*Generador de numero aleatorio entre 0 y 1*/
@@ -38,7 +49,7 @@ void llenar(int *red, int n, float p){
     float test;
     int s;
     s=n*n;
-    for (i=0;i<s;i=i+1){
+    for (i=0;i<s;i++){
 	   test = ( (float)(rand() % 101) )/100 ;
 	   /*printf("Numero aleatorio entre 0 y 1 es %.2f\n",test)*/; 
        /*Acepto o rechazo con probabilidad p*/
@@ -49,6 +60,8 @@ void llenar(int *red, int n, float p){
     }    
 }
 
+//2)Imprimir
+
 void imprimir(int *red, int n){
     int j;
     int i;
@@ -58,11 +71,167 @@ void imprimir(int *red, int n){
     for (i=0;i<s;i=i+1){
 
         if (j!=n)
-         {printf("%d\t",red[i]);
+         {printf("%2d\t",red[i]);
          j=j+1;}
         else
          {j=1;
-         printf("%d\n",red[i]);}
+         printf("%2d\n",red[i]);}
     }
     printf("\n");
+}
+
+//3)Hoshen Kopelman
+
+    //Funcion que implementa en algoritmo de Hoshen-Kopelman.
+    //Recibe el puntero que apunta a la red y asigna etiquetas 
+    //a cada fragmento de red. 
+
+int hoshen(int *red, int n){
+int *clase;
+int i,j,si,sa;    
+int frag;
+
+clase=malloc(n*n*sizeof(int)); /*vector clase*/
+
+
+//Primer Pasada
+
+// primer elemento de la red
+
+  si=0;
+  frag=1;
+  if (*red) 
+    {
+    frag=actualizar(red,clase,si,frag);
+    }
+
+// primera fila de la red
+
+  for(i=1;i<n;i++) 
+    {
+      if (*(red+i)) 
+         {
+           si=*(red+i-1);  
+           frag=actualizar(red+i,clase,si,frag);
+         }
+    }  
+
+
+// el resto de las filas 
+
+  for(i=n;i<n*n;i=i+n)
+    {
+
+      // primer elemento de cada fila
+
+      if (*(red+i)) 
+         {
+           sa=*(red+i-n); //miro el de arriba 
+           frag=actualizar(red+i,clase,sa,frag);
+         }
+
+      for(j=1;j<n;j++)
+         if (*(red+i+j))
+            {
+               si=*(red+i+j-1); 
+               sa=*(red+i+j-n);
+
+               if (sa*si>0)
+               {
+               etiqueta_falsa(red+i+j,clase,sa,si);
+               }
+               else 
+               { if (si!=0) {frag=actualizar(red+i+j,clase,si,frag);}
+                 else       {frag=actualizar(red+i+j,clase,sa,frag);}
+               }
+            }
+    }
+
+//Segunda Pasada
+corregir_etiqueta(red,clase,n);
+
+free(clase);
+
+return 0;
+}
+
+//4)Actualizar
+
+    // Esta funcion actualiza la etiqueta del elemento actual. 
+    // Si es necesario crea una nueva etiqueta y actualiza el vector clase.
+
+int actualizar(int *red, int *clase, int s, int frag){
+/*Pongo Etiqueta*/
+  if(s==0)
+    {  frag=frag+1; //preparo la nueva etiqueta
+     *(red)=frag;
+     *(clase+frag)=frag;//actualizo el vector de clase
+    }
+  else
+    {
+      *(red)=s;
+    } 
+
+return frag;
+}
+
+//5)Etiqueta falsa
+       
+   //Esta función  soluciona el conflicto que aparece cuando si.sa>0.
+   //Busca las etiquetas verdaderas de cada valor, y de ellas se queda con la mas chica.
+   //Asigna la etiqueta mas chica al elemento de la red actual.
+   //Actualiza el vector de clase. 
+
+int etiqueta_falsa(int *red, int *clase, int sa, int si){ 
+//Busco las etiquetas verdaderas de sa y de si en el vector clase
+while (clase[sa]<0){
+    sa=-clase[sa];
+}
+while (clase[si]<0){
+    si=-clase[si];
+}
+
+// De esas dos se queda con la mas chica y esa es la que pone en *r actual. Actualizo clase
+if(sa>si){
+    *red=si;
+    clase[sa]=-si;
+    clase[si]=si;
+}
+else if(sa<si){
+    *red=sa;
+    clase[si]=-sa;
+    clase[sa]=sa;
+}            
+else{//son iguales
+    *red=sa; //cualquiera de las dos
+    clase[si]=si;
+    clase[sa]=sa;
+}
+return 0;
+}
+
+//6)Corregir etiqueta
+  
+  //Esta función toma el vector clase y recorre la red reasignando
+  //etiquetas cuando estas resultan ser falsas.
+int corregir_etiqueta(int *red, int *clase, int n){
+int i,j,s,etq;
+for(i=0;i<n;i++){
+    for (j=0;j<n;j++)
+    {
+        s=red[i*n+j];
+        if (s==0)
+        {
+        red[i*n+j]=0;
+        }
+        else 
+        {
+        etq=clase[s];
+        while (etq<0){
+         etq=clase[-1*etq];
+         }
+        red[i*n+j]=etq;} 
+    }
+}
+return 0;
 }
